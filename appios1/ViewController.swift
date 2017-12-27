cbacf7b74ad7b8d04a1539ed01d9a2acbced7b75
sbpinilla <sbpinilla@hotmail.com>
2017-12-26 22:30:34 -0500

    Commit inicial

---
+++ appios1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appios1
+//
+//  Created by Sergio on 26/12/17.
+//  Copyright © 2017 SbPinilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
